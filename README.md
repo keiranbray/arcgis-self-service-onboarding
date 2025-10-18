66f127f72ec754d1fdbb38a2c31f9b9271d074c4
Jake Hanson <hansonwj@protonmail.com>
2025-10-19 09:15:27 +1300

    Deploy api with static web app, use REST API to improve performance

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
 # arcgis-alternative-signin-web
+
+This app allows ArcGIS administrators and power users to create QR codes which, when scanned,
+add a user to a group and automatically redirects the user to a specified app.
+
+If configured, it also allows users to sign up for an account in the configured organisation.
+
+## Setup
+
+1. Fork this repo into Github, Azure Devops or another git repository hosting service
+
+### Azure static web app
+
+- Go to portal.azure.com
+- Go to static web apps and Create
+- Give your static web app a name (reference the portal/agol you are creating it for) 
+and assign it to a resource group (used to group resources together in billing reports)
+- Under Source, select the github repo you forked above
+- Under app location enter `./web`
+- Under api location enter `./api`
+- Leave output location as `.`
+
+- Deploy the static web app.
+- Note the URL in the top right of the overview page, you will use it later
+
+### ArcGIS
+
+1. Create an oauth app in the portal/agol you want to associate this app with - follow these steps
+https://developers.arcgis.com/documentation/security-and-authentication/user-authentication/tutorials/create-oauth-credentials-user-auth/
+No specific permissions are required.
+For the redirect url, get the url to your static web app (as deployed above) and add `/callback.html` on the end.
+
+2. Deploy a hosted feature table using the zipped file geodatabase in the repo. This will be the config table used to define the QR codes.
+
+
+### Github (or other hosting service)
+
+In the index.html page there are some placeholders ``__CALLBACK_URL__`, `__PORTAL_URL__` and `__CLIENT_ID__`. You can either manually replace these or setup the 
+Github actions to replace them for you based on repository-level environment variables you set.
+
+If you decide to include the string replacement in your pipeline build, Set up environment variables for your repo. For `Github` the workflow is as follows:
+Under Settings > Secrets and variables > Actions, create an environment (I named mine "prod"). Create three entries under Variables:
+- CALLBACK_URL - as in the arcgis section above
+- CLIENT_ID - oauth client id created in arcgis section above
+- PORTAL_URL - this is the full url i.e. https://www.arcgis.com or https://maps.example.com/portal
+
+Add the following code to the yaml file under .github\workflows (see example in repo)
+
+```
+    environment: prod
+    env:
+      CALLBACK_URL: ${{ vars.CALLBACK_URL }} 
+      CLIENT_ID: ${{ vars.CLIENT_ID }} 
+      PORTAL_URL: ${{ vars.PORTAL_URL }}
+
+    - uses: actions/checkout@v3
+        with:
+          submodules: true
+          lfs: false
+
+    steps:
+      # 🔧 Replace placeholders in index html script
+      - name: Replace CLIENT ID placeholder
+        run: |
+          for file in $(find web -type f -name "*.html"); do
+            sed -i "s|__CLIENT_ID__|${CLIENT_ID}|g" "$file"
+          done
+      - name: Replace CALLBACK URL placeholder
+        run: |
+          for file in $(find web -type f -name "*.html"); do
+            sed -i "s|__CALLBACK_URL__|${CALLBACK_URL}|g" "$file"
+          done
+      - name: Replace PORTAL URL placeholder
+        run: |
+          for file in $(find web -type f -name "*.html"); do
+            sed -i "s|__PORTAL_URL__|${PORTAL_URL}|g" "$file"
+          done
+```
+
+### Azure
+We also need to add the following environment variables into azure static web apps
+
+| Variable name | Purpose |
+|---------------|---------|
+|CALLBACK_URL | as in the arcgis section above|
+|CLIENT_ID | oauth client id created in arcgis section above|
+|PORTAL_URL | this is the full url i.e. https://www.arcgis.com or https://maps.example.com/portal. Don't include a forward slash at the end.|
+|MGR_USER | username of user with permissions to create users ("Security and Infrastructure" or "Add user" privileges) and group manager or owner of group users will be added to |
+|MGR_PWORD | password of above user|
+|CONFIG_LAYER_ID | id of config table in your portal|
+
+
+## Usage
+- Add a row into the config table.
+- Enter the group id you want users to be automatically added to, and the url they should be redirected to on sign in
+- Optionally add the user type and role id that users using the create user workflow will be assigned.
+
+- Take the globalid from the row you want to use and add it as the "id" parameter after your url. Right click the browser and click create qr code.
+- Send out the qr code!
---
+++ api/function_app.py
@@ -0,0 +1,488 @@
+import logging
+import os
+import json
+import traceback
+import azure.functions as func
+import requests
+
+
+PORTAL = os.getenv("PORTAL_URL")
+MGR_USER = os.getenv("MGR_USER")
+MGR_PWORD = os.getenv("MGR_PWORD")
+CONFIG_LAYER_ID = os.getenv("CONFIG_LAYER_ID")
+CLIENT_ID = os.getenv("CLIENT_ID")
+REDIRECT_URI = os.getenv("CALLBACK_URL")
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="check-permissions", methods=[func.HttpMethod.POST])
+def add_existing_user(req: func.HttpRequest) -> func.HttpResponse:
+    '''Adds an existing ArcGIS user to the group and redirects them to the app'''
+    logging.info(PORTAL)
+    logging.info(MGR_USER)
+    logging.info(CONFIG_LAYER_ID)
+    logging.info(CLIENT_ID)
+    logging.info(REDIRECT_URI)
+
+    logging.info('Python HTTP trigger function processed a request to add a user to a group.')
+    try:
+        body = req.get_json()
+        logging.info(body)
+        code = body.get("code")
+        verifier = body.get("verifier")
+        globalid = body.get("globalid")
+
+        # Get tokens for the admin and the user
+        user_token = get_user_token(PORTAL, CLIENT_ID, code, REDIRECT_URI, verifier)
+        mgr_token = get_grp_mgr_token(MGR_USER, MGR_PWORD, REDIRECT_URI)
+
+        if user_token is None:
+            result = {"message": "Could not get user token"}
+            logging.info(result)
+            return func.HttpResponse(json.dumps(result),
+                                status_code=500)
+        if mgr_token is None:
+            result = {"message": "Could not get admin token"}
+            logging.info(result)
+            return func.HttpResponse(json.dumps(result),
+                                status_code=500)
+        
+        # Get the app details so we know which group to add them to
+        app_details = get_app_details(PORTAL, CONFIG_LAYER_ID, globalid, mgr_token, REDIRECT_URI)
+        if "group_id" not in app_details or 'redirect_uri' not in app_details:
+            result = {"message": "Couldn't get group id or redirect uri from config. Field is missing from config table."}
+            logging.error("Couldn't get group id or redirect uri from config.")
+            return func.HttpResponse(json.dumps(result), status_code=500)
+        group_id = app_details["group_id"]
+
+        # Work out how to add the user to the group
+        username = get_username_from_token(PORTAL, user_token)
+        group_org = get_user_org(PORTAL, mgr_token)
+        user_org = get_user_org(PORTAL, user_token)
+        logging.info(username)
+        logging.info(group_org)
+        logging.info(user_org)
+
+        # If not in same org, invite and accept on their behalf
+        invite = False
+        if user_org is None or user_org != group_org:
+            invite = True
+        message, status_code = add_user_to_group(PORTAL, mgr_token, username, group_id, REDIRECT_URI, invite, user_token)
+
+        result = {
+            "message": message,
+            "redirect_uri": app_details["redirect_uri"]
+        }
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        result = {
+            "message": e
+        }
+        status_code = 500
+
+    return func.HttpResponse(json.dumps(result),
+                             status_code=status_code)
+
+
+@app.route(route="signup", methods=[func.HttpMethod.POST])
+def user_signup(req: func.HttpRequest) -> func.HttpResponse:
+    '''Creates a new user account, adds user to group and redirects them to the app'''
+    logging.info('Python HTTP trigger function processed a request to create a user.')
+    logging.info(PORTAL)
+    logging.info(MGR_USER)
+    logging.info(CONFIG_LAYER_ID)
+    logging.info(CLIENT_ID)
+    logging.info(REDIRECT_URI)
+    try:
+        try:
+            data = req.get_json()
+        except ValueError:
+            result = {
+                "message": "Invalid JSON",
+            }
+            return func.HttpResponse(json.dumps(result), status_code=400)
+        
+        logging.info(data)
+        username = data.get("username")
+        password = data.get("password")
+        given_name = data.get("given_name")
+        family_name = data.get("family_name")
+        email = data.get("email")
+        globalid = data.get("globalid")
+
+        # Get a token for the manager 
+        mgr_token = get_grp_mgr_token(MGR_USER, MGR_PWORD, REDIRECT_URI)     
+        if mgr_token is None:
+            result = {"message": "Could not get admin token"}
+            return func.HttpResponse(json.dumps(result),
+                                status_code=500)
+
+        app_details = get_app_details(PORTAL, CONFIG_LAYER_ID, globalid, mgr_token, REDIRECT_URI)
+
+        for attr in ('group_id', 'user_license_id', 'user_role_id', 'redirect_uri'):
+            if attr not in app_details:
+                result = {"message": f"Couldn't get {attr}. Field is missing from config table."}
+                logging.error("Could not get required details from layer.")
+                return func.HttpResponse(json.dumps(result), status_code=500)
+        group_id = app_details["group_id"]
+
+        if app_details["user_license_id"] in (None, '') or \
+            app_details["user_role_id"] in (None, ''):
+            result = {"message": "Signup is not enabled. Sign in with an existing account or contact an administrator."}
+            logging.error(result)
+            return func.HttpResponse(json.dumps(result), status_code=403)
+
+        new_user, message = create_portal_user(
+            portal_url=PORTAL,
+            token=mgr_token,
+            username=username,
+            password = password,
+            firstname = given_name,
+            lastname = family_name,
+            email = email,
+            role = app_details["user_role_id"],
+            user_type= app_details["user_license_id"],
+            redirect_uri=REDIRECT_URI,
+            group_id = group_id
+        )
+        if new_user:
+            logging.info(new_user)
+            # Add the user to the group
+            message, status_code = add_user_to_group(PORTAL, mgr_token, username, group_id, REDIRECT_URI, False)
+
+            result = {
+                "message": message,
+                "redirect_uri": app_details["redirect_uri"]
+            }
+            return func.HttpResponse(json.dumps(result),
+                                    status_code=status_code)
+        else:
+            result = {
+                "message": f"Could not create new user. {message}. Please contact an administrator.",
+            }
+            logging.error(result)
+            return func.HttpResponse(json.dumps(result),
+                                    status_code=500)
+        
+    except Exception as e:
+        logging.error("General error: %s",
+                          traceback.format_exc())
+        result = {
+                "message": f"An unexpected error occurred: {e}",
+            }
+        return func.HttpResponse(json.dumps(result),
+                                    status_code=500)        
+
+def get_user_token(portal, client_id, code, redirect_uri, verifier):
+
+    token_url = f"{portal}/sharing/rest/oauth2/token"
+    data = {
+        "grant_type": "authorization_code",
+        "client_id": client_id,
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "code_verifier": verifier
+    }
+
+    reqs = requests.post(token_url, data=data, timeout=10)
+    try:
+        data = reqs.json()
+        return data["access_token"]
+    except:
+        logging.error(traceback.format_exc())
+        return None
+
+def get_grp_mgr_token(user, pword, referer):
+    url = f"{PORTAL}/sharing/rest/generateToken"
+    data = {
+        "username": user,
+        "password": pword,
+        "client": "referer",
+        "referer": referer,
+        "expiration": 60,
+        "f": "json"
+    }
+    try:
+        response = requests.post(url, data=data, timeout=10)
+        response.raise_for_status()
+        token_info = response.json()
+        return token_info["token"]
+    except:
+        logging.error(traceback.format_exc())
+        return None    
+
+def get_username_from_token(portal, user_token):
+    try:
+        reqs = requests.get(f"{portal}/sharing/rest/portals/self?f=json&token={user_token}", timeout=10)
+        data = reqs.json()
+
+        username = data["user"]["username"]
+        logging.info(username)
+        return username
+    except:
+        logging.error(traceback.format_exc())
+        return None   
+
+
+def get_app_details(base_url, config_layer_id, globalid, token, redirect_uri):
+    """
+    Gets the record from the config feature layer and returns the attributes.
+    
+    :param base_url: Base URL of your ArcGIS portal (e.g. "https://organization.example.com/<context>")
+    :param config_layer_id: Item ID of the config Feature Service
+    :param globalid: The GlobalID to match
+    :param token: A valid ArcGIS token
+    """
+
+    # Get the feature service url
+    url = f"{base_url}/sharing/rest/content/items/{config_layer_id}"  # get the service URL
+    headers = {"referer": redirect_uri}
+    item_info = requests.get(url, params={"token": token, "f": "json"}, timeout=10).json()
+
+    # create the url, assuming config is table 0 
+    query_url = item_info["url"]+"/0/query"
+
+    params = {
+        "where": f"GlobalID = '{globalid}'",
+        "outFields": "*",
+        "f": "json",
+        "token": token
+    }
+
+    try:
+        response = requests.post(query_url, data=params, headers=headers, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        if not data.get("features"):
+            raise ValueError(f"No features found for GlobalID {globalid}")
+
+        details = data["features"][0]["attributes"]
+        return details
+    except:
+        logging.error(traceback.format_exc())
+        return {}
+
+
+def add_user_to_group(base_url, mgr_token, user, group_id, redirect_uri, invite=False, user_token=None):
+    """
+    Adds a user to a group (or invites them) using ArcGIS REST API.
+    
+    :param base_url: Base URL of your ArcGIS Portal (e.g. https://organization.example.com/<context>)
+    :param token: Valid ArcGIS token
+    :param user: Username to add
+    :param group_id: Group ID
+    :param invite: Whether to send an invitation instead of direct add
+    :return: (message, http_status_code)
+    """
+
+    try:
+        # --- Step 1: Get current group membership
+        members_url = f"{base_url}/sharing/rest/community/groups/{group_id}/users"
+        params = {"f": "json", "token": mgr_token}
+        headers = {"referer": redirect_uri}
+        r = requests.get(members_url, params=params, headers=headers, timeout=10)
+        r.raise_for_status()
+        members = r.json()
+
+        if "error" in members:
+            return f"Error getting group members: {members['error']['message']}", 400
+
+        if user == members.get("owner") or \
+           user in members.get("admins", []) or \
+           user in members.get("users", []):
+            logging.info("User already in group")
+            return "User already in group", 200
+
+        # --- Step 2: Add or invite user
+        if invite:
+            invite_url = f"{base_url}/sharing/rest/community/groups/{group_id}/invite"
+            data = {
+                "users": user,
+                "f": "json",
+                "token": mgr_token
+            }
+            resp = requests.post(invite_url, data=data, headers=headers, timeout=10)
+            resp.raise_for_status()
+            result = resp.json()
+
+            if not result.get("success"):
+                return "User could not be invited. Contact an administrator.", 400
+
+            user_invites_url = f"{base_url}/sharing/rest/community/users/{user}/invitations"
+            params = {"f": "json", "token": user_token}
+            resp = requests.get(user_invites_url, params=params, headers=headers, timeout=10)
+            resp.raise_for_status()
+            result = resp.json()
+            for invite in result["userInvitations"]:
+                if invite["groupId"] == group_id:
+                    user_invites_url = f"{base_url}/sharing/rest/community/users/{user}/invitations/{invite['id']}/accept"
+                    data = {
+                        "f": "json",
+                        "token": user_token
+                    }
+                    resp = requests.post(user_invites_url, data=data, headers=headers, timeout=10)
+                    resp.raise_for_status()
+                    result = resp.json()
+                    if "success" not in result or result["success"] is False:
+                        return "Group invitation could not be accepted"
+                    break
+
+            return "User invited to group", 200
+        else:
+            add_url = f"{base_url}/sharing/rest/community/groups/{group_id}/addUsers"
+            data = {
+                "users": user,
+                "f": "json",
+                "token": mgr_token
+            }
+            resp = requests.post(add_url, data=data, headers=headers, timeout=10)
+            resp.raise_for_status()
+            result = resp.json()
+
+            not_added = result.get("notAdded", [])
+            if user in not_added:
+                logging.info("User could not be added to group")
+                return "User could not be added to the group. Contact an administrator.", 400
+
+            logging.info("User added to group")
+            return "User added to group", 200
+
+    except Exception as e:
+        logging.error("An error occurred adding the user to the group: %s", traceback.format_exc())
+        return f"An error occurred adding the user to the group: {e}. Contact an administrator.", 500
+
+
+def get_user_org(portal_url: str, token: str) -> str:
+    """
+    Determines what org the user is a member of using their token
+
+    Args:
+        portal_url (str): Base portal URL, e.g. "https://myorg.maps.arcgis.com" or "https://portal.domain.com/portal"
+        token (str): User's token
+
+    Returns:
+        str: orgId of the user
+    """
+    # Query current user info
+    url = f"{portal_url}/sharing/rest/community/self"
+    params = {
+        "token": token,
+        "f": "json"
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+
+        user_info = response.json()
+
+        if "error" in user_info:
+            raise RuntimeError(f"Error fetching user info: {user_info['error']}")
+
+        cur_user_org = user_info.get("orgId")
+        logging.info(f"User org: {cur_user_org}")
+
+        return cur_user_org
+    except Exception as e:
+        logging.error("An error occurred getting the org id: %s", traceback.format_exc())
+        return None      
+
+
+def create_portal_user(portal_url: str, token: str,
+                username: str, password: str,
+                firstname: str, lastname: str,
+                email: str, role: str, user_type: str, 
+                redirect_uri: str, group_id: str):
+    """
+    Creates a new user in ArcGIS using the REST API.
+
+    Args:
+        portal_url (str): Base portal URL, e.g., https://organization.example.com/arcgis
+        token (str): Admin token
+        username (str): Username for new user
+        password (str): Password
+        firstname (str): First name
+        lastname (str): Last name
+        email (str): Email address
+        role (str): Role, e.g., 'iAAAAAAAAA', 'iBBBBBBBB'
+        user_type (str): License type, e.g., 'creatorUT', 'viewerUT', etc.
+        redirect_uri: redirect uri used for token referer
+        group_id: group to be added to
+
+
+    Returns:
+        bool: success
+    """
+
+    # Get default credit assignment
+    reqs = requests.get(f"{portal_url}/sharing/rest/portals/self?f=json&token={token}", timeout=10)
+    data = reqs.json()
+    _credits = data.get("defaultUserCreditAssignment")
+    if _credits is None:
+        _credits = -1
+
+    headers = {"referer": redirect_uri}
+    if "arcgis.com" in portal_url:
+        params = {
+            "f": "json",
+            "token": token,
+            "invitationList": json.dumps({
+                "invitations": [
+                    {
+                        "username": username,
+                        "firstname": firstname,
+                        "lastname": lastname,
+                        "fullname": firstname + " " + lastname,
+                        "email": email,
+                        "password": password,
+                        "role": role,
+                        "userLicenseType": user_type,
+                        "groups": group_id,
+                        "userCreditAssignment": _credits,
+                        "userType": "arcgisonly"
+                    }
+                ],
+                "apps": [],
+                "appBundles": [],
+            })
+        }
+        res = requests.post(f"{portal_url}/sharing/rest/portals/self/invite", data=params, headers=headers, timeout=10)
+        resp = res.json()
+        logging.info(resp)
+        if not resp.get("error"):
+            if username in resp["notInvited"]:
+                logging.error("Unable to create " + username)
+                return False, "Username may already exist."
+        else:
+            logging.error("Can't create " + username)
+            return False, resp["error"]["details"] 
+        return True, None  
+
+    else:
+        url = f"{portal_url}/portaladmin/security/users/createUser"
+        data = {
+            "username": username,
+            "password": password,
+            "firstname": firstname,
+            "lastname": lastname,
+            "email": email,
+            "role": role,
+            "userLicenseTypeId": user_type,
+            "f": "json",
+            "token": token
+        }
+
+        try:
+            response = requests.post(url, data=data, headers=headers, timeout=10)
+            response.raise_for_status()
+            result = response.json()
+
+            if "error" in result:
+                logging.error(f"Error creating user: {result['error']['details']}")
+                raise RuntimeError(result)
+            return True, None
+        
+        except Exception as e:
+            logging.error("An error occurred creating the user: %s", traceback.format_exc())
+            return False, e      
